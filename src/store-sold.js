3dfd3323f6204454a4eac8d4e9eac84d41fb98d3
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-02-12 16:16:39 -0600

    refactored store-sold

--- src/store-sold.js
+++ src/store-sold.js
@@ -1,11 +1,22 @@
 ((numberOfSimultaneousRequests = 1) => {
-    const priceCurrency = /"priceCurrency": "([A-Z]+)",/;
+    //  Regexes needed
     //  this prevents the need to parse XML/JSON, but introduces risks of format changing
-    const priceRegex = /"highPrice": "([0-9 .,]+)[,.](\d{2})"/;
+    const RE = {
+        LISTING_URL: /\?(ref=.*)$/,
+        CURRENCY: /"priceCurrency": "([A-Z]+)",/,
+        PRICE: /"highPrice": "([0-9 .,]+)[,.](\d{2})"/,
+    };
+
     //  risky as well, depends on too much classiness
     const cards = Array.from(document.querySelectorAll('.v2-listing-card a.listing-link, a.buyer-card.card'));
-    //  default to no unit
-    let currencyPrefix = '';
+
+    //  try to use correct symbol when reporting, but cache first resolution of currency to symbol
+    let currencySymbol = '';
+
+    //    sum all found prices for the current page
+    let pageTotal = 0;
+
+    //  hope that it's one of the covered currencies!
     const getCurrencyPrefix = (match) => {
         if (match && match.length) {
             switch (match[1]) {
@@ -22,29 +33,33 @@
             }
         }
     };
+
     //  brutishly simple async attempt
     const queue = cards.map(elem => {
         //  url with the required json+ld
-        const url = elem.getAttribute('href').replace(/\?(ref=.*)$/, '?show_sold_out_detail=1');
+        const url = elem.getAttribute('href').replace(RE.LISTING_URL, '?show_sold_out_detail=1');
+
         //  append price (or message) to the little `Sold` badge on each card.
         const updateSoldLabel = price => elem
             .querySelector('.n-listing-card__price, .text-gray span')
             .insertAdjacentHTML('beforeend', `<span style="color:red;margin-left:1em;">${price}</span>`);
+
         //  callback to delay the fetch
         return () => {
-            //  download page
+
             return fetch(url)
                 //  dump response
                 .then(r => r.ok && r.text())
                 .then(html => {
-                    if (!currencyPrefix) {
-                        currencyPrefix = getCurrencyPrefix(html.match(priceCurrency));
+                    if (!currencySymbol) {
+                        currencySymbol = getCurrencyPrefix(html.match(RE.CURRENCY));
                     }
-                    //  try to parse out the price
-                    const priceMatch = html.match(priceRegex);
+
+                    const priceMatch = html.match(RE.PRICE);
+
                     if (priceMatch && priceMatch.length > 1) {
                         pageTotal += parseFloat(`${priceMatch[1]}.${priceMatch[2]}`);
-                        updateSoldLabel(`${currencyPrefix}${priceMatch[1]}`);
+                        updateSoldLabel(`${currencySymbol}${priceMatch[1]}`);
                     } else {
                         //  haven't seen this happen yet, but just in case
                         updateSoldLabel('N/A');
@@ -53,7 +68,13 @@
                 //  catch any unexpected request closures
                 .catch(err => {
                     updateSoldLabel('ERROR');
-                    console.error('Etsy Sold! :: Store Page Error', err);
+                    console.log(
+                        '%cETSY SOLD!',
+                        'background-color:#F56400;color:white;',
+                        '%cStore Page Error',
+                        'font-weight:bold;',
+                        err
+                    );
                 });
         };
     });
@@ -61,10 +82,7 @@
     //    shows the sum of all sold prices on current page in header next to sales count
     const printPageTotal = () => document
                 .querySelector('.flag-body .text-gray-lightest')
-                .insertAdjacentHTML('beforeend', `<span style="color:red;margin-left:1em;">${currencyPrefix}${pageTotal.toFixed(2)} on this page</span>`);
-
-    //    sum all found prices for the current page
-    let pageTotal = 0;
+                .insertAdjacentHTML('beforeend', `<span style="color:red;margin-left:1em;">${currencySymbol}${pageTotal.toFixed(2)} on this page</span>`);
 
     //  this method will take the first request from the stack
     //  and keep calling itself until the stack's empty
